11fcecb4108dbfa297aff003ce438dd867214d4e
Narendra Patil <narendra.patil@ayekart.com>
2025-05-17 17:16:38 +0530

    theme toggler project to understand context api use case

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+import { ThemeProvider } from "./context/Theme";
+function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeProvider value={{ theme, toggleTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
